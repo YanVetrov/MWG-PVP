120cc656c9dfb1526c22fd6a8b3500d24724cb32
Ян Ветров <yan@MacBook-Pro-An.local>
2021-07-16 22:05:40 +0300

    test

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,17 +9,17 @@ module.exports = {
       crypto: false,
     },
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(js)$/,
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: "babel-loader",
+  //       },
+  //     },
+  //   ],
+  // },
   // optimization: {
   //   minimizer: [
   //     new UglifyJSPlugin({
